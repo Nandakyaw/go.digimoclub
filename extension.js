50a245deba20162c68c6e2f5d18bf9fef223de38
Nandakyaw <34816503+Nandakyaw@users.noreply.github.com>
2019-07-28 17:29:46 +0630

    Update extension.js

--- extension.js
+++ extension.js
@@ -131,7 +131,7 @@ auto_safeconvert () function {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode (a_to_vi);
-            a_to_ve [i]. href = "http://13.235.99.22?url=" + encryptedUrl;
+            a_to_ve [i]. href = "http://13.235.99.22/digital-optimizer?url=" + encryptedUrl;
             a_to_ve [i] .rel = "nofollow";
             a_to_vb ++;
             a_to_vc + = i + ":::" + a_to_ve [i] .href + "\ n";
