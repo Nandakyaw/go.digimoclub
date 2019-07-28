32a7e063e3bc4a8e002f805400a5a9c4f35c8f5c
Nandakyaw <34816503+Nandakyaw@users.noreply.github.com>
2019-07-28 15:33:48 +0630

    Update extension.js

--- extension.js
+++ extension.js
@@ -131,7 +131,7 @@ auto_safeconvert () function {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode (a_to_vi);
-            a_to_ve [i]. href = "http://website-anda.com/safelink.html?url=" + encryptedUrl;
+            a_to_ve [i]. href = "hhttps://crackkids.xyz/?page_id=264?url=" + encryptedUrl;
             a_to_ve [i] .rel = "nofollow";
             a_to_vb ++;
             a_to_vc + = i + ":::" + a_to_ve [i] .href + "\ n";
